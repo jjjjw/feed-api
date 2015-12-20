29802a4cefaaad3bee6b89088b0071503fd9c738
jj <jj.weber@gmail.com>
2015-12-20 15:57:11 -0800

    Initial config for babel and pg

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-'use strict'
-
-const config = require('config')
-const koa = require('koa')
-const koaPg = require('koa-pg')
+import config from 'config'
+import json from 'koa-json'
+import jsonBody from 'koa-json-body'
+import koa from 'koa'
+import pg from 'postgres-gen'
+import users from './routes/users'
 
 let app = koa()
-app.use(koaPg(config.get('pg.conStr')))
+
+app.use(jsonBody({ limit: '10kb' }))
+app.use(json())
+
+app.use(function *(next) {
+  this.pg = pg(config.get('pg.conStr'))
+  yield next
+})
+
+app.use(users.routes())
+
 app.listen(config.get('koa.port'))
 console.log('App listening on port ' + config.get('koa.port'))
---
+++ sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (
+  email varchar(80),
+  password varchar(80)
+);
